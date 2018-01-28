5d05474b9914f70638e6c31d95266a6b535eb3c5
MOHANGANDHI <malapati@cs.stonybrook.edu>
2018-01-27 19:11:41 -0500

    image question

---
+++ RGBimage.py
@@ -0,0 +1,23 @@
+import requests
+from PIL import Image
+import numpy as np
+
+def array(r, c):
+    f_l = []
+    for _ in xrange(4):
+        URL = "https://www.random.org/integers/?num={num}&min=1&max=255&col=1&base=10&format=plain&rnd=new".format(num=(r / 2) * (c / 2))
+        value = requests.get(URL)
+        t_l = map(int, value.content.splitlines())
+        f_l.extend(t_l)
+    to_return = []
+    for i in xrange(0, r * c, c):
+        to_return.append(f_l[i:i + c])
+    return to_return
+
+sz = 128
+RGB = np.zeros((sz, sz, 3), 'uint8')
+RGB[..., 0] = array(sz, sz)
+RGB[..., 1] = array(sz, sz)
+RGB[..., 2] = array(sz, sz)
+img = Image.fromarray(RGB)
+img.save('solution.jpeg')
