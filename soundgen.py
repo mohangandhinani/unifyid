4c5cb9abdf4582cca0ba2dc1b312a17874dbccac
MOHANGANDHI <malapati@cs.stonybrook.edu>
2018-01-27 20:57:11 -0500

    c3

--- soundgen.py
+++ soundgen.py
@@ -1,21 +1,18 @@
-def sound_gen():
-    import math, wave, array
-    duration = 3  # seconds
-    freq = 440  # of cycles per second (Hz) (frequency of the sine waves)
-    volume = 100  # percent
-    data = array.array('h')  # signed short integer (-32768 to 32767) data
-    sampleRate = 44100  # of samples per second (standard)
-    numChan = 1  # of channels (1: mono, 2: stereo)
-    dataSize = 2  # 2 bytes because of using signed short integers => bit depth = 16
-    numSamplesPerCyc = int(sampleRate / freq)
-    numSamples = sampleRate * duration
-    for i in range(numSamples):
-        sample = 32767 * float(volume) / 100
-        sample *= math.sin(math.pi * 2 * (i % numSamplesPerCyc) / numSamplesPerCyc)
-        data.append(int(sample))
-    f = wave.open('SineWave_' + str(freq) + 'Hz.wav', 'w')
-    f.setparams((numChan, dataSize, sampleRate, numSamples, "NONE", "Uncompressed"))
-    f.writeframes(data.tostring())
-    f.close()
+import struct
+import wave
 
-print sound_gen()
+from rand_generator import ran_list_generator
+
+
+def sound_gen():
+    l = ran_list_generator(3 * 44100)  # 3sec * samples per second
+    obj = wave.Wave_write("soundgen.wav")
+    obj.setparams((1, 2, 44100, len(l), 'NONE', 'not compressed'))
+
+    for s in l:
+        obj.writeframes(struct.pack('h', s))
+    obj.close()
+
+
+if __name__ == "__main__":
+    sound_gen()
